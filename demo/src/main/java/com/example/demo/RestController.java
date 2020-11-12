5f32a93e8f1aa7c558989cc8776258bc790e4b66
jagadeeshappi <57809398+jagadeeshappi@users.noreply.github.com>
2020-11-12 21:21:22 +0530

    Update RestController.java
    kkk

--- demo/src/main/java/com/example/demo/RestController.java
+++ demo/src/main/java/com/example/demo/RestController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
-public class RestController { 
+public class RestController {   
     @GetMapping("/get")
     
 	public String get()

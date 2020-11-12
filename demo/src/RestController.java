ebfdf1475e7cd9aee671260167e0c02b3efcf989
jagadeeshappi <57809398+jagadeeshappi@users.noreply.github.com>
2020-11-12 21:05:07 +0530

    Add files via upload
    vvv

---
+++ demo/src/RestController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @GetMapping("/get")
+    
+	public String get()
+	{
+		
+		return "hi jagadeesh";
+	}
+}
